23bc398d082cdf9f839d8cc4cf908bd7cdec4ae0
Alex Rueda U <alexruedau@gmail.com>
2019-01-29 19:30:26 +0100

    Variables strings

---
+++ clase2.js
@@ -0,0 +1,13 @@
+var nombre = 'Alex', apellido = 'Rueda';
+
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length;
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+
+var str = nombre.substr(1,2);
+
+var ultimaLetraDelNombre = nombre.substr(-1);
