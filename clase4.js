e4bdb7ed57bc291bf4f8d84bf2a50d9b28ab06ba
Alex Rueda U <alexruedau@gmail.com>
2019-01-29 23:52:50 +0100

    Funciones en JavaScript

---
+++ clase4.js
@@ -0,0 +1,13 @@
+var nombre = 'Alex', edad = 29;
+
+function imprimirEdad(n, e) {
+  console.log(`${n} tiene ${e} anos`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Ali', 30);
+imprimirEdad('Alice', 33);
+imprimirEdad('Alicja', 36);
+imprimirEdad(33, 'Ale');
+imprimirEdad();
+imprimirEdad('Ala');
