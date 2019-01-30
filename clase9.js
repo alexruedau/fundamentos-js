0f1ee7601b2b4d46782174407dd3cbed160102ff
Alex Rueda U <alexruedau@gmail.com>
2019-01-31 00:12:11 +0100

    Comparaciones en JavaScript. Clase 9.

---
+++ clase9.js
@@ -0,0 +1,14 @@
+var x = 4, y = '4';
+// x == y = true
+//x === y = flse
+var alex = {
+  nombre: 'Alex'
+}
+
+var otraPersona = {
+  nombre : 'Alex'
+}
+
+// alex == otraPersona = false
+// alex === otraPersona = false
+// solo es idéntico si igualamaos otraPersona = alex
