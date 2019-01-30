d1eff1eb0e26b009c2eb15d99dc49fda141a56c4
Alex Rueda U <alexruedau@gmail.com>
2019-01-30 19:00:37 +0100

    Objetos. Clase 6

--- clase5.js
+++ clase5.js
@@ -1,8 +1,23 @@
-nombre = 'Alex';
-
-function imprimirNombreEnMayusculas(nombre) {
-  nombre = nombre.toUpperCase();
-  console.log(nombre);
+var alex = {
+  nombre: 'Alex',
+  apellido: 'Rueda',
+  edad: 29
 }
 
-imprimirNombreEnMayusculas(nombre);
+var juli = {
+  nombre: 'Julika',
+  apellido: 'Klepp',
+  edad: 33
+}
+
+
+function imprimirNombreEnMayusculas({ nombre }) {
+  console.log(nombre.toUpperCase());
+}
+
+imprimirNombreEnMayusculas(alex);
+imprimirNombreEnMayusculas(juli);
+imprimirNombreEnMayusculas({ nombre: 'Pepito' });
+//Errores
+//imprimirNombreEnMayusculas();
+//imprimirNombreEnMayusculas({ apellido: 'Pérez' });
