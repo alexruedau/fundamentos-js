e1d7bc3c42f920982776d6d85bb928f9a47acae2
Alex Rueda U <alexruedau@gmail.com>
2019-01-31 01:06:41 +0100

    Condicionales en JS

---
+++ clase10.js
@@ -0,0 +1,34 @@
+var alex = {
+  nombre: 'Alex',
+  apellido: 'Rueda U.',
+  edad: 29,
+  ingeniero: false,
+  cocinero: false,
+  cantante: false,
+  dj: false,
+  guitarrista: false,
+  drone: true
+}
+
+function imprimirProfesiones(persona) {
+  console.log(`${persona.nombre} es:`);
+  if (persona.ingeniero) {
+    console.log('Ingeniero');
+  } else {
+    console.log('No es ingeniero');
+  }
+  if (persona.cocinero) {
+    console.log('Cocinero');
+  }
+  if (persona.cantante) {
+    console.log('Cantante');
+  }
+  if (persona.guitarrista) {
+    console.log('Guitarrista');
+  }
+  if (persona.drone) {
+    console.log('Piloto de Drone');
+  }
+}
+
+imprimirProfesiones(alex);
