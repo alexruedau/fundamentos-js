f398b0f3fd88fb7477186ee00a8b9192e81172d6
Alex Rueda U <alexruedau@gmail.com>
2019-02-02 19:33:59 +0100

    Clases en JavaScript. Clase 25

--- clase21.js
+++ clase21.js
@@ -1,34 +1,30 @@
-function heredaDe(prototipoHijo, prototipoPadre) {
-  var fn = function () {}
-  fn.prototype = prototipoPadre.prototype
-  prototipoHijo.prototype = new fn
-  prototipoHijo.prototype.constructor = prototipoHijo
+class Persona {
+  constructor(nombre, apellido, estatura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.estatura = estatura
+  }
+
+  saludar () {
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+  }
+
+  soyAlto () {
+    return this.estatura >= 1.7
+  }
+
 }
 
-function Persona(nombre, apellido, estatura) {
-  this.nombre = nombre
-  this.apellido = apellido
-  this.estatura = estatura
+class Desarrollador extends Persona {
+  constructor(nombre, apellido, estatura) {
+    super(nombre, apellido, estatura)
+  }
+
+  saludar() {
+    console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador.`)
+  }
 }
 
-Persona.prototype.saludar = function () {
-  console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
-}
-
-Persona.prototype.soyAlto = function () {
-  return this.estatura >= 1.7
-}
-
-function Desarrollador(nombre, apellido) {
-  this.nombre = nombre
-  this.apellido = apellido
-}
-
-heredaDe(Desarrollador, Persona)
-
-Desarrollador.prototype.saludar = function () {
-  console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador.`)
-}
 
 //En JS no hay clases : HAY PROTOTIPOS!!!
 //HERENCIA PROTOTIPAL
