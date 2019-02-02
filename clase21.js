85e57c1af917f295f3f1de21eafaabf123036b2f
Alex Rueda U <alexruedau@gmail.com>
2019-02-02 19:20:17 +0100

    La verdad oculta sobre las clases en JavaScript.Clase 24

--- clase21.js
+++ clase21.js
@@ -1,3 +1,10 @@
+function heredaDe(prototipoHijo, prototipoPadre) {
+  var fn = function () {}
+  fn.prototype = prototipoPadre.prototype
+  prototipoHijo.prototype = new fn
+  prototipoHijo.prototype.constructor = prototipoHijo
+}
+
 function Persona(nombre, apellido, estatura) {
   this.nombre = nombre
   this.apellido = apellido
@@ -12,10 +19,26 @@ Persona.prototype.soyAlto = function () {
   return this.estatura >= 1.7
 }
 
-var alex = new Persona('Alex', 'Rueda U.', 1.70)
-var juli = new Persona('Julika', 'Klepp', 1.65)
+function Desarrollador(nombre, apellido) {
+  this.nombre = nombre
+  this.apellido = apellido
+}
 
-alex.soyAlto()
-juli.soyAlto()
+heredaDe(Desarrollador, Persona)
+
+Desarrollador.prototype.saludar = function () {
+  console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador.`)
+}
+
+//En JS no hay clases : HAY PROTOTIPOS!!!
+//HERENCIA PROTOTIPAL
+
+
+
+// var alex = new Persona('Alex', 'Rueda U.', 1.70)
+// var juli = new Persona('Julika', 'Klepp', 1.65)
+
+// alex.soyAlto()
+// juli.soyAlto()
 
 //alex.saludar()
