0bc0ad06ad353301adc8430c37a729efdcd8095f
Alex Rueda U <alexruedau@gmail.com>
2019-02-03 01:05:15 +0100

    Funciones como parámetros. Clase 26

--- clase21.js
+++ clase21.js
@@ -5,8 +5,15 @@ class Persona {
     this.estatura = estatura
   }
 
-  saludar () {
-    console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+  saludar (fn) {
+    var { nombre, apellido } = this
+    //El código anterior es equivalente a decir:
+    // var nombre = this.nombre
+    // var apellido = this.apellido
+    console.log(`Hola me llamo ${nombre} ${apellido}`)
+    if (fn) {
+      fn(nombre, apellido, false)
+    }
   }
 
   soyAlto () {
@@ -20,21 +27,32 @@ class Desarrollador extends Persona {
     super(nombre, apellido, estatura)
   }
 
-  saludar() {
-    console.log(`Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador.`)
+  saludar(fn) {
+    var { nombre, apellido } = this
+    console.log(`Hola me llamo ${nombre} ${apellido} y soy desarrollador.`)
+    if(fn) {
+      fn(nombre, apellido, true)
+    }
   }
 }
 
+function responderSaludo(nombre,apellido, esDev) {
+  console.log(`Buen día ${nombre} ${apellido}`)
+  if (esDev) {
+    console.log(`Ah mirá, no sabía que eras desarrollador/a`)
+  }
+}
 
 //En JS no hay clases : HAY PROTOTIPOS!!!
 //HERENCIA PROTOTIPAL
 
 
 
-// var alex = new Persona('Alex', 'Rueda U.', 1.70)
-// var juli = new Persona('Julika', 'Klepp', 1.65)
+var alex = new Desarrollador('Alex', 'Rueda U.', 1.70)
+var juli = new Persona('Julika', 'Klepp', 1.65)
 
 // alex.soyAlto()
 // juli.soyAlto()
 
-//alex.saludar()
+alex.saludar(responderSaludo)
+juli.saludar(responderSaludo)
