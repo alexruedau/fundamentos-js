bc10f730067b714e43796a5bc7745b4ae26e0169
Alex Rueda U <alexruedau@gmail.com>
2019-02-04 16:19:28 +0100

    Promesas. Clase 32

--- clase28.js
+++ clase28.js
@@ -3,29 +3,40 @@ const PEOPLE_URL = 'people/:id'
 
 const opts = { crossDomain: true }
 
-function obtenerPersonaje(id, callback) {
-  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-
-  $
-  .get(url, opts, callback)
-    .fail(() => {
-      console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`);
+function obtenerPersonaje(id) {
+  return new Promise((resolve, reject) => {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $
+    .get(url, opts, function (data) {
+      resolve(data)
     })
-  }
+    .fail(() => reject(id))
+  })
+}
+
+function onError (id) {
+  console.log(`Sucedió un error al obtener el personaje ${id}`);
+}
+
+obtenerPersonaje(1)
+.then(function (personaje) {
+  console.log(`El personaje 1 es ${personaje.name}`);
+})
+.catch(onError)
 
 //A ésto se le conoce como "Callback Hell". Lo es.
-obtenerPersonaje(1, function (personaje) {
-  console.log(`Hola, yo soy ${personaje.name}`);
+// obtenerPersonaje(1, function (personaje) {
+//   console.log(`Hola, yo soy ${personaje.name}`);
 
-  obtenerPersonaje(2, function(personaje) {
-    console.log(`Hola, yo soy ${personaje.name}`);
-
-    obtenerPersonaje(3, function(personaje){
-      console.log(`Hola, yo soy ${personaje.name}`);
-
-      obtenerPersonaje(4, function (peronaje) {
-        console.log(`Hola, yo soy ${peronaje.name}`);
-      })
-    })
-  })
-})
+  // obtenerPersonaje(2, function(personaje) {
+  //   console.log(`Hola, yo soy ${personaje.name}`);
+  //
+  //   obtenerPersonaje(3, function(personaje){
+  //     console.log(`Hola, yo soy ${personaje.name}`);
+  //
+  //     obtenerPersonaje(4, function (peronaje) {
+  //       console.log(`Hola, yo soy ${peronaje.name}`);
+  //     })
+  //   })
+  // })
+// })
