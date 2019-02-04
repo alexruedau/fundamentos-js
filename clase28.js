b39afc3db99955b3b9b95bbc38b4e77f613f0f9e
Alex Rueda U <alexruedau@gmail.com>
2019-02-04 12:21:09 +0100

    Manejando el Orden y el Asincronismo en JavaScript. Clase 30

--- clase28.js
+++ clase28.js
@@ -3,15 +3,23 @@ const PEOPLE_URL = 'people/:id'
 
 const opts = { crossDomain: true }
 
-const onPeopleResponse = function (persona) {
-  console.log(`Hola, yo soy ${persona.name}`);
-}
-
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, callback) {
   const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-  $.get(url, opts, onPeopleResponse)
+
+  $.get(url, opts, function (persona) {
+    console.log(`Hola, yo soy ${persona.name}`);
+
+    if (callback) {
+      callback()
+    }
+  })
 }
 
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+//A ésto se le conoce como "Callback Hell". Lo es.
+obtenerPersonaje(1, function () {
+  obtenerPersonaje(2, function() {
+    obtenerPersonaje(3, function(){
+      obtenerPersonaje(4)
+    })
+  })
+})
